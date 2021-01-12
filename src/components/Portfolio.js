215ad492b184c3d0b0f24d96bbc4bd90f8b0af3e
Suresh <C5110355@acsind.com>
2021-01-12 14:06:51 +0530

    added cards for portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,35 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Gallery from "react-grid-gallery";
-import "firebase/storage";
-import { firebase } from "../firebase/firebase";
-import Loader from "./Loader";
+import React from "react";
 import CardItem from "./CardItem";
 
 const Portfolio = () => {
-  const storage = firebase.storage();
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    storage
-      .ref("/config/galleryimages.json")
-      .getDownloadURL()
-      .then((url) => {
-        setLoading(true);
-        fetch(url, {
-          method: "GET",
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            setImages(data);
-            setLoading(false);
-          })
-          .catch((err) => {
-            console.log(err);
-            setLoading(false);
-          });
-      });
-  }, [storage]);
 
   return (
     <div className="content anchor content__portfolio" id="portfolio">
