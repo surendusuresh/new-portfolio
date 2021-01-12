e353b952ae18966a3bf9e5cf7de7cc798d474ba1
Suresh <C5110355@acsind.com>
2021-01-12 14:00:15 +0530

    changed portfolio to include cards

---
+++ src/components/CardItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+const CardItem = (props) => (
+  <div className="use-bootstrap">
+    <Card style={{ width: "30rem" }} className="mb-4 mx-auto">
+      <Card.Img variat="top" src={props.imgURL} />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>{props.desc}</Card.Text>
+        <Card.Link
+          href={props.siteURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Explore
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  </div>
+);
+
+export default CardItem;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Gallery from "react-grid-gallery";
 import "firebase/storage";
 import { firebase } from "../firebase/firebase";
-import Loader from "../components/Loader";
+import Loader from "./Loader";
+import CardItem from "./CardItem";
 
 const Portfolio = () => {
   const storage = firebase.storage();
@@ -33,7 +34,26 @@ const Portfolio = () => {
   return (
     <div className="content anchor content__portfolio" id="portfolio">
       <h2 className="content__header">Portfolio</h2>
-      {loading ? <Loader /> : <Gallery images={images} />}
+      <div className="portfolio-cards">
+        <CardItem
+          imgURL="https://firebasestorage.googleapis.com/v0/b/new-portfolio-3502a.appspot.com/o/images%2Fgallery%2Farchitect-site.png?alt=media&token=a8f0da59-1854-4af5-8ca8-233a197fd48d"
+          title="Architect Firm Portfolio Website"
+          desc="Website created using React for an architecture firm"
+          siteURL="https://architect-site.herokuapp.com/"
+        />
+        <CardItem
+          imgURL="https://firebasestorage.googleapis.com/v0/b/new-portfolio-3502a.appspot.com/o/images%2Fgallery%2Fshopper.PNG?alt=media&token=d91edb7e-264e-4293-a7cf-602204988d4e"
+          title="eCommerce Web Application"
+          desc="Created using MERN stack which has payment gateway integration"
+          siteURL="https://ssuresh-ecommerce-front.herokuapp.com/"
+        />        
+        <CardItem
+          imgURL="https://firebasestorage.googleapis.com/v0/b/new-portfolio-3502a.appspot.com/o/images%2Fgallery%2Fexpensemanager_login.PNG?alt=media&token=586880a9-08ed-462b-94cf-3993bd463b58"
+          title="Expense Manager App"
+          desc="Created using React and Firebase to track the expenses incurred"
+          siteURL="https://react-expensify-ssuresh.herokuapp.com/"
+        />
+      </div>
     </div>
   );
 };
